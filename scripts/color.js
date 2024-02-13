cb651c819b3eb0a5180161d8e0b92ce71aba5e3a
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-02-13 10:35:59 +0600

    hover effect event  added

---
+++ scripts/color.js
@@ -0,0 +1,37 @@
+function card1InColor(){
+    document.getElementById("card-1").style.backgroundColor = "#FBBCED";
+}
+function card1OutColor(){
+    document.getElementById("card-1").style.backgroundColor = "white";
+}
+function card2InColor(){
+    document.getElementById("card-2").style.backgroundColor = "#69C7F0";
+}
+function card2OutColor(){
+    document.getElementById("card-2").style.backgroundColor = "white";
+}
+function card3InColor(){
+    document.getElementById("card-3").style.backgroundColor = "#FBBCED";
+}
+function card3OutColor(){
+    document.getElementById("card-3").style.backgroundColor = "white";
+}
+function card4InColor(){
+    document.getElementById("card-4").style.backgroundColor = "#69C7F0";
+}
+function card4OutColor(){
+    document.getElementById("card-4").style.backgroundColor = "white";
+}
+function card5InColor(){
+    document.getElementById("card-5").style.backgroundColor = "#FBBCED";
+}
+function card5OutColor(){
+    document.getElementById("card-5").style.backgroundColor = "white";
+}
+function card6InColor(){
+    document.getElementById("card-6").style.backgroundColor = "#69C7F0";
+}
+function card6OutColor(){
+    document.getElementById("card-6").style.backgroundColor = "white";
+}
+
